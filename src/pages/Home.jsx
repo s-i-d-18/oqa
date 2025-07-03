e761c32f43646f2c530bd520f29ad0b039edb347
Siddhant <kambojsiddhant12@gmail.com>
2025-07-03 18:28:41 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import HomePageSlider from "../components/HomePageSlider";
 import GameCard from "../components/GameCard";
 import Subreddits from "../components/Subreddits";
